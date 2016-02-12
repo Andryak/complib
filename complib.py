79882310295c79d7609bacf040f632f419700136
aquina <aquina@andreas-mbp.mobile.usilu.net>
2016-02-12 12:08:55 +0100

    first commit

---
+++ complib.py
@@ -0,0 +1,231 @@
+import itertools
+import random
+from collections import namedtuple
+
+
+UNDEF = None
+Output = namedtuple('Output', ['value', 'steps'])
+
+
+class Function():
+    def __init__(self, name, f):
+        self.name = name
+        self.f = f
+        
+    @classmethod
+    def from_graph(cls, name, graph):
+        """
+            Returns a function by its finite graph.
+            
+            The graph is represented as a set of triples of the form <i,o,t> 
+            meaning that the function on input `i` returns `o` in `t` steps.
+        """
+        m = {}
+        for i,o,t in graph:
+            assert(i >= 0 and o >= 0 and t >= 1)
+            m[i] = Output(o, t)
+        def inner(n):
+            try:
+                result = m[n]
+            except KeyError:
+                result = UNDEF
+            return result
+        return Function(name, inner)
+
+    def graph(self, start, offset):
+        """
+            Returns the graph of this function only considering inputs in the 
+            range [`start`, `start`+`offset`).
+        """
+        g = set()
+        for i in xrange(start, start+offset):
+            output = self.f(i)
+            if output is not UNDEF:
+                g.add((i, output.value, output.steps))
+        return g
+
+    def domain(self, start=0, offset=10):
+        """
+            Returns the domain of this function only considering inputs in the
+            range [`start`, `start`+`offset`).
+        """
+        g = self.graph(start, offset)
+        return set([i for i,_,_ in g])
+
+    def range(self, start=0, offset=10):
+        """
+            Returns the range of this function only considering inputs in the
+            range [`start`, `start`+`offset`).
+        """
+        g = self.graph(start, offset)
+        return set([o for _,o,_ in g])
+
+    def pp_graph(self, start=0, offset=10):
+        """
+            Prints the graph of this function only considering inputs in the
+            range [`start`, `start`+`offset`).
+        """
+        for i in xrange(start, start+offset):
+            output = self.f(i)
+            print "{}({}) =".format(self, i),
+            if output is not UNDEF:
+                print "{} in {} steps".format(output.value, output.steps)
+            else:
+                print "undefined"
+
+    def __call__(self, i, steps=None, on_undef="abort"):
+        """
+            Returns the output of running this function on input `i` for `steps` steps.
+            
+            If the behaviour of this function is undefined for input `i`, then 
+            if `on_undef` is set to "abort" the program is terminated, otherwise
+            None is returned.
+        """
+        assert(i >= 0)
+        assert(steps is None or steps >= 1)
+        
+        output = self.f(i)
+        if output is UNDEF and steps is None:
+            print "{}({}) runs forever!".format(self, i)
+            if on_undef == "abort":
+                exit(1)
+        elif output is UNDEF and steps is not None:
+            return None
+        elif output.steps <= steps:
+            return output.value
+
+        return None
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+
+def T3(f, i, t):
+    """
+        Kleene's predicate of arity three.
+        Returns `True` iff f(i) terminates in at most `t` steps.
+    """
+    output = f(i)
+    if output is UNDEF:
+        return False
+    return (output.steps <= t)
+
+
+def T4(f, i, o, t):
+    """
+        Kleene's predicate of arity four.
+        Returns `True` iff `f(i) outputs `o` in at most `t` steps.
+    """
+    output = f(i)
+    if output is UNDEF:
+        return False
+    return (output.value == o and output.steps <= t)
+
+
+def dovetailing(f, stop_predicate):
+    """
+        Runs function f on all inputs for any number of steps.
+        Whenever the function terminates on input i producing o within t steps 
+        the predicate stop_predicate(i,o,t) is executed. If it returns `True`
+        this function stops and returns `True`, otherwise this function keeps 
+        running potentially forever.
+    """
+    for t in itertools.count(1):
+        for i in xrange(t):
+            print "Running function {} on input {} for {} steps.".format(f, i, t),
+            o = f(i, steps=t)
+            if o is not None:
+                print "It halts returning {}.".format(o)
+                if stop_predicate(i,o,t):
+                    print "Stop predicate returned `True`, thus dovetailing ends."
+                    return True
+            else:
+                print "It does not halt."
+
+
+def dovetailing_on_pairs(f, stop_predicate):
+    """
+        Runs function f on all possible pairs of inputs for any number of steps.
+        Whenever the function terminates on inputs i and j producing outputs oi 
+        and oj within t steps the predicate stop_predicate(i,oi,j,oj,t) is executed. 
+        If it returns `True` this function stops and returns `True`, otherwise 
+        this function keeps running potentially forever.
+    """
+    for t in itertools.count(1):
+        for i in xrange(t):
+            for j in xrange(t):
+                print "Running function {} on inputs {} and {} for {} steps.".format(f, i, j, t),
+                oi = f(i, steps=t)
+                oj = f(j, steps=t)
+                if oi is None and oj is None:
+                    print "It does not halt on either input."
+                elif oi is None:
+                    print "It does not halt on input {}.".format(i)
+                elif oj is None:
+                    print "It does not halt on input {}.".format(j)
+                elif stop_predicate(i, oi, j, oj, t):
+                    print "Halts on both."
+                    print "Stop predicate returned `True`, thus dovetailing ends."
+                    return True
+                else:
+                    print "Halts on both, but stop predicate returned `False`."
+
+
+# =============
+# | Encodings |
+# =============
+
+def pair2number(a,b):
+    d = a+b
+    return (d*(d+1))/2 + a
+
+def number2pair(n):
+    if n == 0:
+        return (0,0)
+    a,b = number2pair(n-1)
+    if b == 0:
+        return (0, a+1)
+    return (a+1, b-1)
+
+def pair2first(n):
+    return number2pair(n)[0]
+
+def pair2second(n):
+    return number2pair(n)[1]
+
+
+# =======================
+# | Function generators |
+# =======================
+
+# Generator of constant functions.
+def K(k, steps=1, name=None):
+    """
+        Returns the constant `k` function, that is, a function 
+        that returns `k` on each input in `steps` steps.
+    """
+    assert(steps >= 1)
+    if name is None:
+        name = "K{}_{}_steps".format(k, steps)
+    return Function(name, lambda n: Output(k, steps))
+
+
+# =====================
+# | Utility functions |
+# =====================
+
+# Always undefined function.
+undef = Function("undef", lambda n: UNDEF)
+
+# Identity function.
+ident = Function("ident", lambda n: Output(n,1))
+
+# Successor function.
+succ = Function("succ", lambda n: Output(n+1, 1))
+
+# Constant functions.
+K0 = K(0, name="K0")
+K1 = K(1, name="K1")
---
+++ test.py
@@ -0,0 +1,116 @@
+if __name__ == '__main__':
+    import complib
+    import itertools
+    
+
+    # f(0) = 1 in 3 steps
+    # f(1) = 7 in 1 steps
+    # f(n>1) = undefined
+    f = complib.Function.from_graph(
+        name="f",
+        graph=set([
+            (0,1,3),
+            (1,7,1)
+        ])
+    )
+
+    # g(0) = 5 in 1 steps
+    # g(n>0) = undefined
+    g = complib.Function.from_graph(
+        name="g",
+        graph=set([
+            (0,5,1)
+        ])
+    )
+
+    # Print the graphs of all functions in the range [0,10).
+    print "Graph of function f:"
+    f.pp_graph()
+    
+    print "Graph of function g:"
+    g.pp_graph()
+    
+    print "Graph of function K0:"
+    complib.K0.pp_graph()
+    
+    print "Graph of function ident:"
+    complib.ident.pp_graph()
+    
+    print "Graph of function undef:"
+    complib.undef.pp_graph()
+
+    # Extract the graphs of all functions in the range [0,3).
+    print "Graph of function f:", f.graph(0,3)
+    print "Graph of function g:", g.graph(0,3)
+    print "Graph of function K0:", complib.K0.graph(0,3)
+    print "Graph of function ident:", complib.ident.graph(0,3)
+    print "Graph of function undef:", complib.undef.graph(0,3)
+    
+    # Print the output value of all functions on input 0.
+    print "f(0) =", f(0)
+    print "g(0) =", g(0)
+    print "K0(0) =", complib.K0(0)
+    print "ident(0) =", complib.ident(0)
+    
+    # This would run forever, setting on_undef to "continue" does not terminate
+    # the program but is not a realistic behaviour. It is impossibile in general
+    # to understand whether a program will terminate on a given input or not.
+    print "undef(0) =",
+    complib.undef(0, on_undef="continue")
+    
+    # A bunch of semi-deciders for sets of programs.
+    
+    # Semi-deciders for the set { i | 7 in cod(phi_i) }.
+    def sd1_1(f):
+        for t in itertools.count(1):
+            for k in range(0, t):
+                print "Running function {} on input {} for {} steps.".format(f, k, t)
+                output = f(k, steps=t)
+                if output is not None and output == 7:
+                    print "On this input it returns 7!"
+                    return True
+    
+    def sd1_2(f):
+        return complib.dovetailing(f, lambda i,o,t: o == 7)
+
+    # Semi-deciders for the set { (i,y) | exists a,b. a != b, phi_i(a) halts and phi_i(a) = phi_i(b) }.
+    def sd2_1(f):
+        s = set()
+        for t in itertools.count(1):
+            for k in range(0, t):
+                print "Running function {} on input {} for {} steps.".format(f, k, t)
+                output = f(k, steps=t)
+                if output is not None:
+                    # If this output was produced in the past by running `f` on
+                    # a different input, output `True` and stop.
+                    for inv, outv in s:
+                        if inv != k and outv == output:
+                            print "This function returns {} on inputs {} and {}!".format(output, inv, k)
+                            return True
+                    # Otherwise, record that `f` outputs `output` when ran on input k.
+                    print "Recording that {}({}) = {}.".format(f, k, output)
+                    s.add((k, output))
+                else:
+                    print "{}({}) did not terminate in {} steps.".format(f, k, t)
+
+    def sd2_2(f):
+        return complib.dovetailing_on_pairs(f, lambda i,oi,j,oj,t: i != j and oi == oj)
+    
+    # Apply the semi-deciders to some functions.
+    sd1_1(f)
+    sd1_2(f)
+    sd1_1(complib.ident)
+    sd1_2(complib.ident)
+    
+    # k12_3_steps(i) = 12 in 3 steps.
+    k12_3_steps = complib.K(12, 3)
+
+    sd2_1(k12_3_steps)
+    sd2_2(k12_3_steps)
+
+    # Test encodings of the pairs.
+    print "Encoding and decoding of pairs as natural numbers:"
+    for i in xrange(10):
+        p = complib.number2pair(i)
+        n = complib.pair2number(*p)
+        print i, "->", p, "->", n
